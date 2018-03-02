8070f6b95eca56ecad9b098a4459f594925876a5
Duncan MacDonald <dmacdonald@Duncans-MacBook-Pro.local>
2018-03-02 11:34:43 -0800

    debugging

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def move():
 
 @app.route("/end", methods=["POST"])
 def end():
-    return "false", 200
+    return OK, 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, use_reloader=True)
